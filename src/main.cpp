ff7efe88e6a1a835ea95bec706098dda3fa6fdd6
Den-Prahms-ihr-Ivo <ivo@ivolino.de>
2026-02-08 16:40:17 +0100

    initial PlatformIO project + first succesfull flash

---
+++ src/main.cpp
@@ -0,0 +1,25 @@
+#include <Arduino.h>
+
+// put function declarations here:
+int myFunction(int, int);
+bool myFlad = false;
+
+void setup() {
+  Serial.begin(115200);
+  delay(2000);                 // gives Serial a moment to come up
+  Serial.println("Hello World");
+
+  // put your setup code here, to run once: 
+  int result = myFunction(2, 3);
+
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
+
+// put function definitions here:
+int myFunction(int x, int y) {
+  return x + y;
+}
